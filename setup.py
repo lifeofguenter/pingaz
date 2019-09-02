a3e56674960739add772ec2e3cb7f26b5d9bbd0b
gunter.grodotzki <gunter.grodotzki@tidal.com>
2019-09-02 14:25:53 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='pingaz',
-    version='0.0.5',
+    version='0.0.6',
     author='Gunter Grodotzki',
     author_email='gunter@grodotzki.co.za',
     description='Monitor latency between AZs with fping and cloudwatch.',
